26ca1ccc8d72394b1f83bd45fc8df3daf09aaecd
Pedro <a22005821@alunos.ulht.pt>
2022-05-28 19:56:35 +0100

    Create Save system for save and load

---
+++ SaveSystem.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 
+{
+    public class SaveSystem
+    {
+        public void Save(Board board){
+            string map;
+            for (int i = 0; i < 5 ;i++){
+                for (int j = 0; i < 5; i++){
+                    map+= Board.map[i,j]
+                }
+            } 
+
+        }
+
+        public void Load(Board board){
+            string map = ReadAllText(SaveFile.txt);
+            for (int i = 0; i < 5 ;i++){
+                for (int j = 0; i < 5; i++){
+                    
+                }
+            } 
+
+        }
+    }
+}
