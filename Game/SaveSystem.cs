007f309c0ce4601227b640897c3dde8095a77dd4
Pedro <a22005821@alunos.ulht.pt>
2022-05-29 16:12:04 +0100

    Add load game function to SaveSystem

--- Game/SaveSystem.cs
+++ Game/SaveSystem.cs
@@ -59,5 +59,78 @@ namespace Game
 
             return playerString;
         }
+
+        public bool Load(Board board)
+        {
+            if(File.Exists(path))
+            {
+                string[] save = File.ReadAllLines(path);
+
+                string[] map = save[0].Split("/");
+                string[] player1 = save[1].Split("/");
+                string[] player2 = save[2].Split("/");
+
+                for (int i = 0; i < 5 ;i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        board.Map[i,j] = String2Tile(map[i*5 +j], board, board.ArrayToBoard(i,j));
+                    }
+                }
+
+                board.players[0] = String2Player(player1);
+                board.players[1] = String2Player(player2);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private Player String2Player(string[] s)
+        {
+            Player player = new Player(s[4]);
+
+            player.X = int.Parse(s[0]);
+            player.Y = int.Parse(s[1]);
+            player.CheatDice = bool.Parse(s[2]);
+            player.ExtraDice = bool.Parse(s[3]);
+
+            return player;
+        }
+
+        private Tile String2Tile(string s, Board board, int index)
+        {
+            Tile tile;
+            switch(s)
+            {
+                case "🚀":
+                    tile = new Boost(board);
+                    break;
+                case "🎲":
+                    tile = new CheatDice(board);
+                    break;
+                case "🟥":
+                    tile = new Cobra(board);
+                    break;
+                case "➕":
+                    tile = new ExtraDice(board);
+                    break;
+                case "冃":
+                    tile = new Ladders(board);
+                    break;
+                case "🐍":
+                    tile = new Snake(board);
+                    break;
+                case "↺":
+                    tile = new UTurn(board);
+                    break;
+                default:
+                    tile = new Tile( board, index.ToString(), false);
+                    break;
+            }
+
+            return tile;
+        }
     }
 }
